214f2a9471d378e2d78de6d52705658e0a085316
kakynchik <ukakyn@gmail.com>
2025-02-21 19:37:07 +0200

    Refactor Program class to include namespace and improve code organization

--- firstdz.cs
+++ firstdz.cs
@@ -1,26 +1,29 @@
-class Program
+namespace _16_11
 {
-    static void Main()
+    class Program
     {
-        Task1 task1 = new Task1();
-        task1.Execute();
-        
-        Task2 task2 = new Task2();
-        task2.Execute();
-        
-        Task3 task3 = new Task3();
-        task3.Execute();
+        static void Main()
+        {
+            Task1 task1 = new Task1();
+            task1.Execute();
 
-        Task4 task4 = new Task4();
-        task4.Execute();
+            Task2 task2 = new Task2();
+            task2.Execute();
 
-        Task5 task5 = new Task5();
-        task5.Execute();
+            Task3 task3 = new Task3();
+            task3.Execute();
 
-        Task6 task6 = new Task6();
-        task6.Execute();
+            Task4 task4 = new Task4();
+            task4.Execute();
 
-        Task7 task7 = new Task7();
-        task7.Execute();
+            Task5 task5 = new Task5();
+            task5.Execute();
+
+            Task6 task6 = new Task6();
+            task6.Execute();
+
+            Task7 task7 = new Task7();
+            task7.Execute();
+        }
     }
 }
